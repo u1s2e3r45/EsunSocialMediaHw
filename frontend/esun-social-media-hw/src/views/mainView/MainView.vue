435a3a073fec74c1ba7e7f581e1f6fd1a808d9c9
DESKTOP-C18L5ON\xhenr <xhenryx468@gmail.com>
2026-05-05 21:02:31 +0800

    frontend first step

--- frontend/esun-social-media-hw/src/views/mainView/MainView.vue
+++ frontend/esun-social-media-hw/src/views/mainView/MainView.vue
@@ -1,6 +1,115 @@
-<script lang="ts"></script>
+<script lang="ts">
+import { RouterLink } from 'vue-router';
+</script>
 <template>
-  <div>Main Page</div>
+    <div class="content">
+      <div class="topbar">
+        <nav>
+          <RouterLink to="/login">login</RouterLink>
+        </nav>
+      </div>
+      <div class="middle">
+        <div v-if="true" class="allpost">
+          <div class="post">
+            <div class="userName">
+              <H1>尼哥</H1>
+            </div>
+            <div class="createdAt">
+              <H2>2026年05月05日</H2>
+            </div>
+            <div>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi sint, praesentium ullam mollitia quod asperiores iste, odit odio, natus tempora veniam nobis quaerat magnam ea dicta facere quibusdam rerum at obcaecati. Sapiente error cupiditate sunt! Molestias perspiciatis enim ab minus eaque adipisci repudiandae incidunt sapiente vel! Provident architecto nisi, amet, repudiandae deleniti aspernatur deserunt sed, similique odit quam voluptatum eaque numquam praesentium harum voluptatem? Reiciendis illum consequatur, amet dicta adipisci, culpa ut aspernatur a, maiores nihil mollitia cum? Dolorem eveniet modi ullam quis doloribus omnis. Recusandae, fuga eos nemo neque fugit accusamus, vero quibusdam iure mollitia earum illum enim? Eaque expedita iste, quisquam voluptatem, corrupti rem quos beatae culpa laborum neque facilis reiciendis enim debitis hic nam numquam ex natus nihil! Quis impedit aspernatur eveniet illo quas perspiciatis praesentium accusamus laborum nulla, omnis rerum expedita nisi nam? Ratione sed neque quis et! Corporis maxime soluta asperiores doloribus reiciendis expedita earum, nostrum hic blanditiis quaerat et provident commodi voluptatibus voluptas minus quos consequuntur impedit ipsam repellat! Cumque, commodi beatae suscipit quidem aspernatur eos odio omnis! Non, dolorem mollitia pariatur necessitatibus vero modi soluta voluptates odit dignissimos delectus aliquid ipsa sunt sequi labore doloribus libero deleniti fuga! Maxime voluptates, voluptas, nihil recusandae, voluptatem ipsam est placeat non accusamus tempore voluptate ducimus quisquam explicabo deleniti atque consequatur ratione sit incidunt laborum pariatur aliquid libero natus. Laboriosam pariatur culpa possimus! Totam odit ex, ut perspiciatis consequatur obcaecati sit, quam inventore hic perferendis modi consequuntur beatae suscipit vitae repudiandae? Quia similique harum totam, ad expedita quasi repellendus odio voluptate hic velit obcaecati labore quos nulla, facilis vero reprehenderit. Culpa repudiandae sit reiciendis expedita necessitatibus quasi vitae ut, nulla quam hic delectus incidunt corrupti, earum consequatur aut ad animi accusantium pariatur magnam quibusdam iste eveniet repellendus. Ab maxime, cumque ex laborum eum nulla sequi. Distinctio, facere ipsa neque unde ipsum amet fugiat maiores quae nesciunt, nihil nostrum odio provident natus adipisci! Temporibus ex veritatis natus, alias ea vel libero deleniti adipisci sint aspernatur reiciendis et atque provident? Provident officiis consequuntur obcaecati debitis nihil cupiditate optio quos, nobis totam saepe nemo vitae et omnis odit odio explicabo harum accusamus aliquid, tempora consequatur numquam sunt. Consequuntur minima minus temporibus fuga voluptatibus eaque sed quo necessitatibus nesciunt alias veniam optio voluptate doloribus atque placeat dolores sunt non odio, nihil, hic esse iusto vitae doloremque. Dicta ratione minima dignissimos commodi quaerat ullam porro consequatur vitae eum incidunt sunt at illum cumque, hic nesciunt rem accusamus. Temporibus eum, ipsa qui minima ad voluptatibus necessitatibus, quae doloremque sequi repellat unde voluptates ut voluptatem, totam ipsum sunt! Magnam tempore laboriosam nisi qui a nesciunt atque possimus dolorum officiis rerum, culpa iusto et aperiam eveniet illum odit animi eligendi voluptates unde eum. Veniam illum dolorum vel vero cupiditate debitis rerum recusandae, commodi eius, amet fugiat quia perferendis ipsa animi placeat ea voluptate quisquam numquam iure? Unde, officiis asperiores laudantium voluptate fugit ducimus quos cumque. Eos id neque iusto qui iste eligendi culpa similique omnis officia, ullam ad, dolorum molestiae ea eum! Modi, minima sed.
+            </div>
+            <div class="postimg" v-if="true">
+              <img src="/public/1.jpg" alt="">
+            </div>
+          </div>
+          <div class="post">
+            <div class="userName">
+              <H1>尼哥</H1>
+            </div>
+            <div class="createdAt">
+              <H2>2026年05月05日</H2>
+            </div>
+            <div>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt, deserunt ipsum incidunt dolores et voluptatum, reiciendis vel voluptatibus totam, aperiam tenetur cumque cum quas esse repudiandae! Quos ex quibusdam magni expedita excepturi perferendis corrupti voluptatibus repellendus? Voluptates beatae, eum reiciendis mollitia ipsa ea obcaecati est nesciunt minima quidem fugit ducimus, neque recusandae cum? Nemo tenetur quia quos, corporis eius sequi totam sapiente, odio voluptatem commodi cupiditate iure, omnis sit pariatur possimus exercitationem! Impedit a, neque sint dolor blanditiis nobis delectus officia perferendis, rerum ipsa nisi debitis iste libero incidunt placeat reiciendis, perspiciatis consequuntur quaerat vitae vero. Adipisci harum earum molestiae unde, quasi omnis blanditiis? Corrupti consectetur possimus, eos architecto dolor fugit qui. Nemo dolorem possimus, modi dolore aliquam distinctio cumque explicabo nesciunt odio, soluta pariatur maxime reiciendis at nihil, error ratione vel porro omnis natus aliquid expedita commodi. Vel, ipsum sapiente itaque praesentium similique nesciunt delectus. Ipsum aut voluptate fuga recusandae illum voluptas fugiat sit nisi ratione veritatis? Fugit tempora blanditiis, quis tenetur culpa a veritatis vel voluptatibus asperiores nobis corrupti molestiae quo possimus eligendi delectus corporis labore numquam eum. Exercitationem deserunt iste porro, similique eum enim delectus amet suscipit? Quasi non sunt enim at animi temporibus voluptatibus natus repellendus, aperiam, repudiandae, est officiis quibusdam quod tempora necessitatibus quia sint recusandae libero beatae magnam dolor eligendi incidunt inventore soluta. Saepe iusto fuga consequuntur, error laudantium omnis nesciunt ab, repellendus, nam dolorem illum ex eligendi adipisci. Adipisci officiis harum rerum quo porro quibusdam non repellat! Sunt eum qui perferendis reiciendis, nulla error iusto voluptates. Aliquid sunt placeat cum possimus veritatis repellat ea fugiat aut magnam maxime, eaque odit quaerat veniam. Excepturi dolor minima esse exercitationem officiis praesentium odio optio voluptatum facilis ad! Temporibus ad suscipit eos quod illum fugit hic vitae perferendis dignissimos nam. Doloribus quis ab nam corporis ullam? Est quibusdam ipsa tenetur, quisquam quis doloribus harum quo reiciendis provident vitae odio quidem dicta repellendus eos incidunt impedit doloremque natus in mollitia officia veniam? Perspiciatis vitae nam minus quis porro neque, praesentium, earum non amet delectus odio. Hic doloremque, modi voluptate aliquam rerum vitae reiciendis, repellat deserunt soluta voluptas quis. Vitae quo vero, ducimus deserunt cupiditate nostrum dignissimos eius tempore eligendi rerum dolorem in corporis et commodi necessitatibus harum eaque! Quas velit rem tempora voluptatibus commodi, beatae asperiores quisquam corrupti repellat mollitia repudiandae error, tenetur quibusdam aspernatur provident, ullam ex magni? Asperiores hic nisi tempora neque laborum eaque architecto repellendus inventore ipsa voluptatum quibusdam veniam expedita laboriosam earum distinctio, iure non vitae atque reiciendis officiis magni aspernatur saepe harum delectus. Labore, autem corrupti ab dignissimos pariatur laudantium modi saepe dolor, doloremque aut fuga corporis consequuntur ipsam aspernatur enim porro. Tenetur quis assumenda aperiam et voluptate aliquid, alias cumque quisquam libero dolores maiores sint in at quae amet molestiae, delectus perspiciatis nostrum itaque. Modi deleniti vero praesentium obcaecati nisi expedita porro alias, nulla nemo nam consequuntur voluptates soluta ab reprehenderit maxime aliquam quod magni distinctio blanditiis? Sed quaerat quod consequuntur ipsam, quo dolore tempora cupiditate porro illo accusamus exercitationem, perferendis deserunt?
+            </div>
+            <div class="postimg" v-if="true">
+              <img src="/public/2.png" alt="">
+            </div>
+          </div>
+          <div class="post">
+            <div class="userName">
+              <H1>尼哥</H1>
+            </div>
+            <div class="createdAt">
+              <H2>2026年05月05日</H2>
+            </div>
+            <div>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti odio consectetur ea itaque, rerum consequatur nemo quaerat. Sint vero quos assumenda neque vitae deserunt iste eligendi explicabo commodi placeat molestiae quasi voluptas rerum numquam delectus officiis ea dolorem cupiditate quae, tempora omnis quia? Obcaecati sequi repellendus aperiam exercitationem, laudantium nam doloribus temporibus possimus praesentium nihil reiciendis veritatis! Maxime autem illo cupiditate animi odio rerum tenetur doloremque similique! Repellendus optio hic eaque tenetur asperiores repellat illo delectus vero, odio impedit! Corrupti obcaecati ex ducimus qui rem voluptatibus asperiores numquam dicta odio eum cum at quisquam dolorem sed nostrum recusandae et blanditiis, debitis, nulla quidem voluptas nihil? Soluta ratione consequuntur alias! Hic dolores maiores delectus. Repellendus dicta sint excepturi illo ad aut iste impedit, atque vel expedita hic illum corrupti, sed voluptatem pariatur suscipit dignissimos perferendis maiores veritatis qui harum, cumque nemo. Excepturi, quibusdam quos modi blanditiis, eveniet quam eaque dolorem incidunt eligendi dolore culpa quis repudiandae repellendus, hic neque et expedita? Aliquam ducimus quas, repudiandae ut qui voluptatum reiciendis perspiciatis sunt, quis suscipit sapiente consequatur similique esse sequi modi accusantium laudantium repellat labore voluptates tenetur consequuntur dignissimos. Veniam ullam magnam minus accusantium quos voluptate provident nemo cum temporibus aperiam quasi, nesciunt, fugit reiciendis, ad labore maiores harum. Assumenda, ratione animi eius consequatur facere quidem enim debitis repudiandae fuga laudantium alias voluptatum delectus ducimus optio labore quibusdam incidunt similique maiores libero aut accusamus architecto illum? Nemo numquam a adipisci cumque est, reiciendis quaerat? Quasi repudiandae veniam eveniet id nisi quo ipsum in, porro quos libero et ullam, sequi voluptatem, magni natus nemo quam earum. Illo expedita voluptatem, cum quidem fugit itaque aliquid distinctio dolores, magni culpa delectus! Sunt voluptates quibusdam cupiditate mollitia eveniet reprehenderit magni, laborum voluptatibus tempore maxime voluptatem autem excepturi, voluptate expedita suscipit doloremque ut vero atque. Vitae, voluptates aut obcaecati nisi cum ut enim doloribus autem consequatur temporibus maiores ratione inventore commodi quaerat provident possimus iste esse facere. Officia quo vero blanditiis praesentium reiciendis velit saepe, vel voluptate recusandae maxime, possimus labore dolore rerum odit consectetur maiores optio. Quisquam quod debitis modi voluptate, in nisi tempore dicta nostrum, sed officia commodi fugit, reprehenderit dolore eaque excepturi. Facilis, obcaecati accusamus. Sunt dolor molestias laudantium perferendis voluptates nobis vitae ipsam corporis ab, ea laborum soluta reiciendis omnis ipsum quasi asperiores natus nam repellendus, officiis eaque aperiam! Inventore non quam, quos assumenda dolore soluta pariatur possimus expedita, vero eius omnis corrupti blanditiis quaerat aut praesentium a. In eveniet ad, eum laudantium, cumque aliquid molestias repudiandae architecto, praesentium eaque culpa vitae magnam omnis sequi ipsa magni saepe sapiente. Quisquam officia aliquam fugiat eius ducimus consequatur similique adipisci eos aspernatur quibusdam, atque placeat labore dolor in illum pariatur deleniti quas blanditiis excepturi totam, mollitia praesentium? Fugit, hic in saepe deleniti temporibus repellat iusto. Eaque, tenetur dolore nesciunt culpa fugiat ipsam harum et modi ex porro, aut saepe sed quibusdam quo animi? Commodi libero laudantium, sunt non similique debitis quod. Dolor eum similique autem dolorem aliquid esse maiores aperiam? Corporis reiciendis a asperiores ea repudiandae!
+            </div>
+            <div class="postimg" v-if="true">
+              <img src="/public/3.png" alt="">
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
 </template>
 
-<style></style>
+<style scoped>
+  .content{
+    height: auto; /* 100% 視窗高度 */
+    width: 100%;
+    margin: 0;
+    padding: 0;
+    border-style: dotted;
+    border-color: green;
+    /* display: inline-block;
+    width: 200vh;
+    height: 100vh; */
+    /* background-color: aqua; */
+  }
+  .topbar{
+    margin: 2rem;
+    width: 100vw;
+    height: auto;
+    background-color: red;
+  }
+  .middle{
+    overflow: visible hidden;
+    color: black;
+    width: 90%;
+    height: auto;
+    background-color: yellow;
+    display: flex; 
+    justify-content: center; 
+    
+  }
+  nav{
+    width: 100%;
+    font-size: large;
+    text-align: right;
+    margin-top: 2rem;
+  }
+  .allpost{
+    display: inline-block;
+  }
+  .post{
+    margin: 5rem;
+    border-style: dotted;
+    border-color: green;
+  }
+  img{
+    width: 30%;
+    height: auto;
+    max-height: 30%;
+  }
+  /* 
+  nav a.router-link-exact-active {
+    color: var(- -color-text);
+  }
+  nav a.router-link-exact-active:hover-{
+    background-color: transparent;
+  } */
+</style>
